d596c31629514a3ced0493df67e2c9e961638360
amirmhmd <amirmhmd@amir.amir>
2025-07-29 19:44:05 +0330

    basic information about data file checked.

---
+++ src/main.jl
@@ -0,0 +1,12 @@
+using Pkg
+Pkg.add(["DataFrames", "CSV", "Dates", "Statistics", "Plots", "GLM"])
+
+using DataFrames, CSV, Dates, Statistics, Plots, GLM
+
+df = CSV.read("/home/amirmhmd/code/electricity-predictor-julia/data/PJME_hourly.csv", DataFrame)
+
+println("Shape of data: ", size(df))
+println("Column's names: ", names(df))
+println(first(df, 10))
+println(describe(df))
+
